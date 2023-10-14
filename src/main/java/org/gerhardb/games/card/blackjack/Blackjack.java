1977984b385046eb3668e9d832515454d4a63fc5
gtbec <gtbec@10.0.0.34>
2023-10-14 12:24:21 -0400

    Finish Java 21

--- src/main/java/org/gerhardb/games/card/blackjack/Blackjack.java
+++ src/main/java/org/gerhardb/games/card/blackjack/Blackjack.java
@@ -9,19 +9,19 @@ import org.gerhardb.games.card.deck.Deck;
  * 
  	The Object Of The Game: to get as close to or equal to 21 without going over.
 	The Rules Are As Follows:
-	• There will be only 2 players – a “human” player and a dealer
-	• The players are each dealt 2 cards to start the hand
-	• The player can choose to hit one or more times, or stand with any amount
-	• The dealer must hit if his cards total less than 17 and stand otherwise
-	• If the player’s or dealer’s cards total over 21, they bust and their turn is over
-	• If either player has 21 with their first two cards, they win (unless they both have 21 on their first two cards, in which case it is a tie)
-	• If both players bust, the dealer wins
-	• If both players have the same score, they tie
-	• The player always takes their turn before the dealer
-	• All cards count as their face value, except A which can be 1 or 11 and J, Q, K all count as 10
-	• The deck should be shuffled before each game
-	• You do not need to implement advanced blackjack rules (split, double or insurance)
-	• Only one deck will be used per game
+	- There will be only 2 players a human player and a dealer
+	- The players are each dealt 2 cards to start the hand
+	- The player can choose to hit one or more times, or stand with any amount
+	- The dealer must hit if his cards total less than 17 and stand otherwise
+	- If the player's or dealer's cards total over 21, they bust and their turn is over
+	- If either player has 21 with their first two cards, they win (unless they both have 21 on their first two cards, in which case it is a tie)
+	- If both players bust, the dealer wins
+	- If both players have the same score, they tie
+	- The player always takes their turn before the dealer
+	- All cards count as their face value, except A which can be 1 or 11 and J, Q, K all count as 10
+	- The deck should be shuffled before each game
+	- You do not need to implement advanced blackjack rules (split, double or insurance)
+	- Only one deck will be used per game
  *
  * @author Gerhard
  *
@@ -41,7 +41,7 @@ public class Blackjack implements Game
 		Deck deck = Deck.get52CardDeck();
 		deck.shuffle();		
 		
-		// There will be only 2 players – a “human” player and a dealer
+		// There will be only 2 players ï¿½ a ï¿½humanï¿½ player and a dealer
 		Hand player = new Hand();
 		Hand dealer = new Hand();
 		Decider.Outcome outcome = dealCards(deck, player, dealer);
@@ -55,7 +55,7 @@ public class Blackjack implements Game
 			return;			
 		}
 		
-		//  If the player’s or dealer’s cards total over 21, they bust and their turn is over
+		//  If the playerï¿½s or dealerï¿½s cards total over 21, they bust and their turn is over
 		while(player.getScore() < 22)
 		{
 			// The player can choose to hit one or more times, or stand with any amount
@@ -96,7 +96,7 @@ public class Blackjack implements Game
 		System.out.println("The dealer is showing: " + dealer.getUpCard());
 		System.out.println("Your hand is: " + player);		
 		
-		//  If the player’s or dealer’s cards total over 21, they bust and their turn is over
+		//  If the playerï¿½s or dealerï¿½s cards total over 21, they bust and their turn is over
 		if (player.getScore() > 21)
 		{
 			return Decider.whoWon(player.getScore(), dealer.getScore());
@@ -106,7 +106,7 @@ public class Blackjack implements Game
 	
 	static Decider.Outcome dealerPlays(Deck deck, Hand player, Hand dealer)
 	{
-		//  If the player’s or dealer’s cards total over 21, they bust and their turn is over
+		//  If the playerï¿½s or dealerï¿½s cards total over 21, they bust and their turn is over
 		if (player.getScore() > 21)
 		{
 			return Decider.whoWon(player.getScore(), dealer.getScore());
